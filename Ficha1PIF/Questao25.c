6c8833cf9a2562e51a3c0ff9f2cfa18914b96a50
Renato Veras <109183573+deverasrenato@users.noreply.github.com>
2026-08-26 21:20:39 +0000

    Create Questao25.c

---
+++ Ficha1PIF/Questao25.c
@@ -0,0 +1,18 @@
+/* =========================================================================
+ * Desenho da Letra "R" no Console
+ * -------------------------------------------------------------------------
+ * Este programa utiliza um único printf e sequências de escape '\n' para 
+ * desenhar a letra "R" estilizada em formato de bloco no console.
+ * ========================================================================= */
+
+#include <stdio.h>
+
+int main() {
+    printf("RRRR \n");
+    printf("R   R\n");
+    printf("RRRR \n");
+    printf("R  R \n");
+    printf("R   R\n");
+
+    return 0; // Finaliza o programa com sucesso
+}
