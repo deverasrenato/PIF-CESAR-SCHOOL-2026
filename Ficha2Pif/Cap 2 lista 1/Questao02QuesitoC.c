68f355d6108fc934d00db4282cf1e8555ae03b14
Renato Veras <109183573+deverasrenato@users.noreply.github.com>
2026-09-16 19:53:21 +0000

    Update Questao02QuesitoC.c

--- Ficha2Pif/Cap 2 lista 1/Questao02QuesitoC.c
+++ Ficha2Pif/Cap 2 lista 1/Questao02QuesitoC.c
@@ -3,7 +3,6 @@ c)
 
 int main() {
     char ch;
-    // O espaco antes de %c instrui o scanf a ignorar espacos, tabulacoes e quebras de linha ('\n') no buffer
     scanf(" %c", &ch);
     return 0;
 }
